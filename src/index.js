7f7c7524a79b6f76b6c656982a186964bc514578
Adam Marc William <marc@rhythm.digital>
2016-12-09 09:47:04 +0000

    Page 28

--- src/index.js
+++ src/index.js
@@ -1,22 +1,52 @@
 import PIXI from 'pixi.js';
-import scaleToWindow from './helpers/scaleToWindow.js'
 
-let renderer = PIXI.autoDetectRenderer(256, 256);
+//Aliases
+let Container = PIXI.Container,
+    autoDetectRenderer = PIXI.autoDetectRenderer,
+    loader = PIXI.loader,
+    Texture = PIXI.Texture,
+    TextureCache = PIXI.utils.TextureCache,
+    Sprite = PIXI.Sprite,
+    Rectangle = PIXI.Rectangle;
 
+//Create a Pixi stage and renderer and add the
+//renderer.view to the DOM
+let stage = new Container(),
+    renderer = autoDetectRenderer(256, 256);
 document.body.appendChild(renderer.view);
 
-let stage = new PIXI.Container();
+//Set the canvas's border style and background color
+renderer.view.style.border = "1px dashed black";
+renderer.backgroundColor = "0xFFFFFF";
 
-renderer.backgroundColor = 0xfff666;
-renderer.resize(512, 512);
-renderer.view.style.border = '1px dashed black';
-renderer.view.style.position = 'absolute';
-renderer.view.style.width = window.innerWidth + 'px';
-renderer.view.style.height = window.innerHeight + 'px';
-renderer.view.style.display = 'block';
+function frame(source, x, y, width, height) {
+  let texture;
+  let imageFrame;
 
-window.addEventListener('resize', e => {
-	scaleToWindow(renderer.view, 0xfff666)
-})
+  if (typeof source == 'string') {
+    if (TextureCache[source]) {
+      texture = new Texture(TextureCache[source]);
+    }
+  } else if (source instanceof Texture) {
+    texture = new Texture(source);
+  } if (!texture) {
+    console.log(`Please load the ${source} texture into cache.`)
+  } else {
+    imageFrame = new Rectangle(x, y, width, height);
+    texture.frame = imageFrame;
+    return texture;
+  }
+}
 
-renderer.render(stage);
+loader
+  .add('/images/tileset.png')
+  .load(setup);
+
+function setup () {
+  let adventuress = new Sprite(frame('/images/tileset.png', 160, 256, 32, 32))
+  adventuress.x = 64;
+  adventuress.y = 64;
+  adventuress.scale.set(3, 3);
+  stage.addChild(adventuress);
+  renderer.render(stage);
+}
